f56b126822ad6b776764bdf3a5c9f784dd8769ee
Mugamba669 <brunohectre@gmail.com>
2022-02-21 19:32:43 +0300

    created the client demo for flutter app lw-server

---
+++ src/components/Input.vue
@@ -0,0 +1,35 @@
+<template lang="html">
+    <div>
+        <input type="text" v-model="input" @onchange="checkInput" placeholder="Provide the url here"/>
+    </div>
+</template>
+<script>
+export default {
+    name:'Input',
+    data(){
+        return{
+            input:""
+        }
+    },
+    methods:{
+        checkInput(){
+                this.$emit('checkNewInput',this.input);
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+    div{
+       
+        input{
+            width:200px;
+            margin: 10px;
+            outline: none;
+            padding: 10px;
+            border-radius: 5px;
+            border: 1px solid #63ccdd;
+            font: 200 18px Ubuntu,Arial;
+            
+        }
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Input
+    @checkNewInput="newInput"
+    />
+    <button @click="connectServer">Connect</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Input from "@/components/Input.vue";
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+   Input,
+  },
+  methods:{
+    connectServer(){
+      var socket = new WebSocket("wss://www.nowviba.com/music/index.php");
+      socket.onopen =(event)=>{
+        console.log(event)
+      }
+    },
+    newInput(value){
+        console.log(value);
+    }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+      .home{
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-content: center;
+        button{
+          margin: 10px;
+          padding:10px;
+          font: 500 18px Ubuntu,Arial;
+        }
+      }
+</style>
